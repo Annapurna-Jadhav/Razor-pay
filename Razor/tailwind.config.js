0d1a886403b0a166759f57e3c84b052a5c714e72
Annapurna <jadhavannapurna37@gmail.com>
2025-01-14 19:49:44 +0530

    update file

---
+++ Razor/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serid"]
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+      
+      },
+
+    },
+    plugins: [],
+  }
+}
